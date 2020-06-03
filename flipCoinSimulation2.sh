ab93791ecc071ee00502b0e70a428c1998c17c7b
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-03 20:07:16 +0530

    Added: Count Upto 21 and winning Head Tail Functionality.

--- flipCoinSimulation2.sh
+++ flipCoinSimulation2.sh
@@ -2,12 +2,10 @@ echo "Welcome to Flip coin simulaiton 1"
 
 declare -A result
 
-read -p "Enter the number of flip: " num
 
-for (( i=0; i<$num; i++ ))
+while [[ ${result['Head']} -ne 21 && ${result['Tail']} -ne 21 ]]
 do
 	coin=$(( RANDOM%2 ))
-
 	if [[ $coin -eq 0 ]]
 	then
 		result['Head']=$(( ${result['Head']}+1 ))
@@ -18,3 +16,13 @@ done
 
 echo "Head win: " ${result['Head']}" Times"
 echo "Tail Win: " ${result['Tail']}" Times"
+
+if [ ${result['Head']} -eq 21 ]
+then
+	echo "Head Win by :"$(( ${result['Head']} - ${result['Tail']} ))
+elif [ ${result['Tail']} -eq 21 ]
+then
+	echo "Tail Win by :"$(( ${result['Tail']} - ${result['Head']} ))
+else
+	echo "Tie"
+fi
