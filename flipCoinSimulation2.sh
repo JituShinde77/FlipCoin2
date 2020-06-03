b6d8de8556656d6b04bbc283bfd0108f9d080d1f
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-03 19:43:55 +0530

    Added: Winning count Head Tail Functionality.

--- flipCoinSimulation2.sh
+++ flipCoinSimulation2.sh
@@ -1,9 +1,20 @@
 echo "Welcome to Flip coin simulaiton 1"
 
-coin=$(( RANDOM%2 ))
-if [[ $coin -eq 0 ]]
-then
-	echo "Winner is Head"
-else
-	echo "Winner is Tail"
-fi
+declare -A result
+
+read -p "Enter the number of flip: " num
+
+for (( i=0; i<$num; i++ ))
+do
+	coin=$(( RANDOM%2 ))
+
+	if [[ $coin -eq 0 ]]
+	then
+		result['Head']=$(( ${result['Head']}+1 ))
+	else
+		result['Tail']=$(( ${result['Tail']}+1 ))
+	fi
+done
+
+echo "Head win: " ${result['Head']}" Times"
+echo "Tail Win: " ${result['Tail']}" Times"
